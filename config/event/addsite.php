6621c4489e923b9938e3f78a130c5c81a1f14dac
chaiwat20180 <chaiwat20180@gmail.com>
2024-09-18 05:34:43 +0700

    Update addsite.php

--- config/event/addsite.php
+++ config/event/addsite.php
@@ -88,28 +88,3 @@
         }
     }
 ?>
-
-<script>
-    $(document).ready(function(){ 
-        <?php if(isset($_SESSION['username'])){?>$('#modalNotification').modal();<?php } ?> 
-        $('div.modal button').click(function(){ location.href='home.php'; });
-    });
-    </script>
-<div class="modal fade" id="modalNotification" tabindex="-1" role="dialog" aria-labelledby="modalNotificationTitle" aria-hidden="true">
-      <div class="modal-dialog modal-dialog-centered" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="modalNotificationTitle">NOTIFICATION</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <p>CONTENT</p>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-          </div>
-        </div>
-      </div>
-    </div>
