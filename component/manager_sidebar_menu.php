61e65a06d18750735fbdd320451f3f63b0dd4f5f
chaiwat20180 <chaiwat20180@gmail.com>
2024-10-05 18:23:10 +0700

    SOMEBUG ?
    SOMEBUG ?

--- component/manager_sidebar_menu.php
+++ component/manager_sidebar_menu.php
@@ -2,57 +2,94 @@
 
         $query_check_permission_manager = $db_connect -> prepare("
                                                                 SELECT 
-                                                                                tbemp.Emp_ID,
-                                                                                tbemp.Emp_FirstName ,
-                                                                                tbemp.Emp_LastName,
-                                                                                tbemp.Dep_ID emp_dep,
-                                                                                manager.Dep_ID as manager_dep,
-                                                                                tbd.Dep_Name,
-                                                                                manager.Emp_ID as Manager,
-                                                                                tbg.Group_ID,
-                                                                                tbg.Group_Name,
-                                                                                tbg.Group_Admin
+                                                                    tbemp.Emp_ID,
+                                                                    tbemp.Emp_FirstName,
+                                                                    tbemp.Emp_LastName,
+                                                                    tbemp.Dep_ID AS emp_dep,
+                                                                    manager.Dep_ID AS manager_dep,
+                                                                    tbd.Dep_Name,
+                                                                    manager.Emp_ID AS Manager,
+                                                                    tbg.Group_ID,
+                                                                    tbg.Group_Name,
+                                                                    tbg.Group_Admin
                                                                 FROM
-                                                                                tbemployee tbemp
+                                                                    tbemployee tbemp
                                                                 LEFT JOIN tbdepartment tbd ON tbd.Dep_ID = tbemp.Dep_ID
-                                                                LEFT JOIN tbemployee manager ON manager.Emp_ID = tbd.Dep_Manager	
+                                                                LEFT JOIN tbemployee manager ON manager.Emp_ID = tbd.Dep_Manager
                                                                 LEFT JOIN tbassign_group tbag ON tbag.Emp_ID = tbemp.Emp_ID
                                                                 LEFT JOIN tbgroup tbg ON tbg.Group_ID = tbag.Group_ID
                                                                 WHERE 
-                                                                            (tbemp.Emp_ID = :Emp_ID AND tbemp.IsDeleted = '0')
-    															OR 
-                                                                			(tbemp.Emp_ID = :Emp_ID AND tbg.Group_Admin = '2');
-
+                                                                    (
+                                                                        tbemp.Emp_ID = :Emp_ID 
+                                                                        AND tbemp.IsDeleted = '0'
+                                                                        AND tbemp.Emp_ID = manager.Emp_ID
+                                                                    )
+                                                                OR 
+                                                                    tbg.Group_ID IN (
+                                                                        SELECT tbag_inner.Group_ID 
+                                                                        FROM tbassign_group tbag_inner
+                                                                        LEFT JOIN tbgroup tbg_inner ON tbg_inner.Group_ID = tbag_inner.Group_ID
+                                                                        WHERE
+                                                                            tbag_inner.Emp_ID = :Emp_ID 
+                                                                            AND tbg_inner.Group_Admin = '2'
+                                                                    )
 
                 ");
         $query_check_permission_manager -> bindParam(':Emp_ID', $_SESSION['Emp_ID']);
         $query_check_permission_manager -> execute();
         $fetch_check_user_permission_manager = $query_check_permission_manager->fetch();
-        if(
-                $fetch_check_user_permission_manager['Emp_ID'] == $fetch_check_user_permission_manager['Manager'] || 
-                $fetch_check_user_permission_manager['Group_Admin'] == '2'  &&
-                $fetch_check_user_permission_manager['emp_dep'] == $fetch_check_user_permission_manager['manager_dep']
-        ){ 
-            $query_count_wait_approve = $db_connect -> prepare("
+        if (    $fetch_check_user_permission_manager && 
+                ($fetch_check_user_permission_manager['Emp_ID'] == $fetch_check_user_permission_manager['Manager'] || 
+                $fetch_check_user_permission_manager['Group_Admin'] == '2')
+            ) { 
+            IF($fetch_check_user_permission_manager['Group_Admin'] == '2'){
+                $query_count_wait_approve = $db_connect -> prepare("
                                                                SELECT 
                                                                     COUNT(tbt.Ticket_ID) as Count_Ticket
-                                                                FROM  
+                                                                FROM 
                                                                     tbticket tbt
-                                                                LEFT JOIN tbemployee tbemp ON tbemp.Emp_ID = tbt.Emp_ID
-                                                                LEFT JOIN tbdepartment tbd ON tbd.Dep_ID = tbemp.Dep_ID
-                                                                LEFT JOIN tbemployee manager ON manager.Emp_ID = tbd.Dep_Manager
-                                                                LEFT JOIN tbdepartment managerDept ON managerDept.Dep_ID = manager.Dep_ID
-                                                                LEFT JOIN tbgroup tbg ON tbg.Group_ID = tbt.Group_ID
-                                                                LEFT JOIN tbassign_group tbag ON tbag.Group_ID = tbg.Group_ID
-
+                                                                INNER JOIN tbemployee emp ON emp.Emp_ID = tbt.Emp_ID
+                                                                INNER JOIN tbdepartment dep ON dep.Dep_ID = emp.Dep_ID
+                                                                INNER JOIN tbgroup tbg ON tbg.Group_ID = tbt.Group_ID
                                                                 WHERE
-                                                                        manager.Emp_ID = :Emp_ID
-
-
+                                                                        tbt.State_ID = 'ST06'
 
                 ");
-            $query_count_wait_approve -> bindParam(':Emp_ID', $_SESSION['Emp_ID']);
-            $query_count_wait_approve -> execute();
+            }
+            else{
+                $query_count_wait_approve = $db_connect->prepare("
+                                                                    SELECT 
+                                                                        COUNT(tbt.Ticket_ID) as Count_Ticket
+                                                                    FROM 
+                                                                        tbticket tbt
+                                                                    INNER JOIN tbemployee emp ON emp.Emp_ID = tbt.Emp_ID
+                                                                    INNER JOIN tbdepartment dep ON dep.Dep_ID = emp.Dep_ID
+                                                                    INNER JOIN tbgroup tbg ON tbg.Group_ID = tbt.Group_ID
+                                                                    WHERE
+                                                                        (
+                                                                            tbt.Emp_ID = :Emp_ID
+                                                                            AND tbt.State_ID = 'ST06'
+                                                                        )
+                                                                        OR 
+                                                                        (
+                                                                            dep.Dep_Manager = :Emp_ID
+                                                                        )
+                                                                        OR 
+                                                                        (
+                                                                            tbg.Group_ID IN (
+                                                                                SELECT tbag.Group_ID 
+                                                                                FROM tbassign_group tbag 
+                                                                                INNER JOIN tbgroup tbg ON tbg.Group_ID = tbag.Group_ID
+                                                                                WHERE tbag.Emp_ID = :Emp_ID
+                                                                                AND tbg.Group_Admin = '2'
+                                                                            )
+                                                                        )
+
+                ");
+                $query_count_wait_approve -> bindParam(':Emp_ID', $_SESSION['Emp_ID']);
+            }
+            
+            @$query_count_wait_approve -> execute();
             $fetch_check_count_wait_approve = $query_count_wait_approve->fetch(PDO::FETCH_ASSOC);
 ?> 
 
